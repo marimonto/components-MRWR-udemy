c032dc3892e408d83f8a3441fcb6441a18054db8
marimontod-kc <mariana.montoya@kinandcarta.com>
2022-09-09 13:47:32 -0500

    passing props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CommentDetail from './components/CommentDetail';
+import { faker } from '@faker-js/faker';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-    <CommentDetail/>
-    <CommentDetail/>
-    <CommentDetail/>
-    <CommentDetail/>
+      <CommentDetail
+        author='Sam'
+        timeAgo='Today at 4:45PM'
+        avatar={faker.image.avatar()}
+        content='Nice blog post!' />
+      <CommentDetail
+        author='Alex'
+        timeAgo='Today at 2:00AM'
+        avatar={faker.image.avatar()}
+        content='I like this subject' />
+      <CommentDetail
+        author='Jane'
+        timeAgo='Yesterday at 5:00PM'
+        avatar={faker.image.avatar()}
+        content='I like the writing' />
     </div>
   )
 }
