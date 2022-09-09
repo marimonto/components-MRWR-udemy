40500d2105b7e77ea4d9a501a10a9103251035e2
marimontod-kc <mariana.montoya@kinandcarta.com>
2022-09-09 15:37:44 -0500

    Implementing an Approval Card and Optional Styling

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CommentDetail from './components/CommentDetail';
 import { faker } from '@faker-js/faker';
+import ApprovalCards from './components/ApprovalCard';
+import CommentDetail from './components/CommentDetail';
+import './style/App.css'
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4:45PM'
-        avatar={faker.image.avatar()}
-        content='Nice blog post!' />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 2:00AM'
-        avatar={faker.image.avatar()}
-        content='I like this subject' />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 5:00PM'
-        avatar={faker.image.avatar()}
-        content='I like the writing' />
+      <div className='ui cards'>
+        <ApprovalCards>
+          <CommentDetail author='Sam'
+            timeAgo='Today at 4:45PM'
+            avatar={faker.image.avatar()}
+            content='Nice blog post!' /></ApprovalCards>
+        <ApprovalCards>
+          <CommentDetail
+            author='Alex'
+            timeAgo='Today at 2:00AM'
+            avatar={faker.image.avatar()}
+            content='I like the subject' /></ApprovalCards>
+        <ApprovalCards>
+          <CommentDetail
+            author='Jane'
+            timeAgo='Yesterday at 5:00PM'
+            avatar={faker.image.avatar()}
+            content='I like the writing' /></ApprovalCards>
+      </div>
+
     </div>
   )
 }
